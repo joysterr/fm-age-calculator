487b616d233cf2a42a9a57f76389d20a11960534
Joyster Rodrigues <77756392+joysterr@users.noreply.github.com>
2024-01-21 22:25:07 +0000

    added toggleError function for cleaner code, added reset input feature

--- scripts/submitForm.js
+++ scripts/submitForm.js
@@ -11,6 +11,12 @@ const inputError = document.querySelectorAll('.input__error')
 
 const today = new Date()
 
+userInput.forEach((input) => {
+    input.addEventListener('click', () => {
+        resetInputs()
+    })
+})
+
 inputForm.addEventListener('submit', (e) => {
     handleSubmit(e)
 })
@@ -26,9 +32,9 @@ function handleSubmit(e) {
 
     console.log(inputData)
 
-    formValidation(inputData)
+    const isInputValid = formValidation(inputData)
 
-    if (formValidation) {
+    if (isInputValid) {
         const output = calc(inputYear, inputMonth, inputDay)
         outputDay.innerHTML = output[2]
         outputMonth.innerHTML = output[1]
@@ -41,36 +47,39 @@ function formValidation(inputData) {
     const { inputDay, inputMonth, inputYear } = inputData
 
     if (inputDay === '' && inputMonth === '' && inputYear === '') {
-        inputLabel.forEach(label => {
-            label.classList.add('error')
-        })
-        userInput.forEach(input => {
-            input.classList.add('error-outline')
-        })
+        toggleError(true)
         inputError.forEach(error => {
             error.innerHTML = 'This field is required'
         })
-        console.log('error')
+        console.log('error: no input')
+        return false
     }
     else if (!isValidDay(inputDay) && !isValidMonth(inputMonth) && !isValidYear(inputYear)) {
+        toggleError(true)
         inputError[0].innerHTML = 'Must be a valid day'
         inputError[1].innerHTML = 'Must be a valid month'
         inputError[2].innerHTML = 'Must be in the past'
+        return false
     }
     else if (!isValidDay(inputDay)) {
+        toggleError(true)
         inputError[0].innerHTML = 'Must be a valid day'
+        return false
     }
     else if (!isValidMonth(inputMonth)) {
+        toggleError(true)
         inputError[1].innerHTML = 'Must be a valid month'
+        return false
     }
     else if (!isValidYear(inputYear)) {
+        toggleError(true)
         inputError[2].innerHTML = 'Must be in the past'
+        return false
     }
     else {
         console.log('valid. all good.')
         return true
     }
-    return false
 }
 
 
@@ -84,4 +93,33 @@ function isValidMonth(inputMonth) {
 
 function isValidYear(inputYear) {
     return (inputYear > today.getFullYear()) ? false : true
+}
+
+function resetInputs() {
+    toggleError(false)
+}
+
+
+function toggleError(boo) {
+    if (boo) {
+        inputLabel.forEach(label => {
+            label.classList.add('error')
+        })
+        userInput.forEach(input => {
+            input.classList.add('error-outline')
+        })
+    } else {
+        userInput.forEach((input) => {
+            input.value = ''
+        })
+        inputLabel.forEach(label => {
+            label.classList.remove('error')
+        })
+        userInput.forEach(input => {
+            input.classList.remove('error-outline')
+        })
+        inputError.forEach(error => {
+            error.innerHTML = '&nbsp;'
+        })
+    }
 }
